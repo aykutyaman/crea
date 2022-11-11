54c59beaca5e39b90936b1de10dedfcaa12e79de
aykut <aykut@jotform.com>
2022-11-11 16:51:42 +0300

    Basic domain objects are created
    - We also created an domain object arbitraries

--- libs/domain/src/lib/domain.spec.ts
+++ libs/domain/src/lib/domain.spec.ts
@@ -1,7 +1,26 @@
-import { domain } from './domain';
+import { getArbitrary } from 'fast-check-io-ts';
+import * as fc from 'fast-check';
+import * as D from './domain';
 
 describe('domain', () => {
-  it('should work', () => {
-    expect(domain()).toEqual('domain');
+  it('can create valid arbitrary data for domain objects', () => {
+    const productsArb = getArbitrary(D.Products);
+    const commentsArb = getArbitrary(D.Comments);
+    const userArb = getArbitrary(D.User);
+
+    fc.assert(
+      fc.property(
+        productsArb,
+        commentsArb,
+        userArb,
+        (products, comments, user) => {
+          return (
+            D.Products.is(products) &&
+            D.Comments.is(comments) &&
+            D.User.is(user)
+          );
+        }
+      )
+    );
   });
 });
--- libs/domain/src/lib/domain.ts
+++ libs/domain/src/lib/domain.ts
@@ -1,3 +1,51 @@
-export function domain(): string {
-  return 'domain';
-}
+import * as t from 'io-ts';
+
+export const ID = t.string;
+export type ID = t.TypeOf<typeof ID>;
+
+export const Score = t.union([
+  t.literal(1),
+  t.literal(2),
+  t.literal(3),
+  t.literal(4),
+  t.literal(5),
+]);
+export type Score = t.TypeOf<typeof Score>;
+
+export const Date = t.string;
+export type Date = t.TypeOf<typeof Date>;
+
+// TODO: distinguish User(logged) and UserLike(not logged)
+export const User = t.type({
+  username: t.string,
+  password: t.string,
+});
+export type User = t.TypeOf<typeof User>;
+
+export const Product = t.type({
+  id: ID,
+  name: t.string,
+  price: t.number,
+  score: Score,
+  image: t.string,
+  description: t.string,
+  arrivalDate: Date,
+});
+export type Product = t.TypeOf<typeof Product>;
+
+export const Products = t.array(Product);
+export type Products = t.TypeOf<typeof Products>;
+
+export const Comment = t.type({
+  id: ID,
+  productId: ID,
+  text: t.string,
+  username: t.string,
+  userId: ID,
+  score: Score,
+  date: Date,
+});
+export type Comment = t.TypeOf<typeof Comment>;
+
+export const Comments = t.array(Comment);
+export type Comments = t.TypeOf<typeof Comments>;
