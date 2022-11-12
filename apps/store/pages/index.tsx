dab256ca99c3a1f0fe8f27f7eb8aea2b9f07b27e
aykut <aykut@jotform.com>
2022-11-12 17:20:01 +0300

    Created products page

--- apps/store/pages/index.tsx
+++ apps/store/pages/index.tsx
@@ -1,56 +1,7 @@
-import { useEffect, useState } from 'react';
-import * as D from '@crea/domain';
 import styles from './index.module.css';
-import { useRouter } from 'next/router';
 
 export function Index() {
-  const [products, setProducts] = useState<D.Products>([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    fetch(`http://localhost:3333/api/products`, {
-      credentials: 'include',
-    })
-      .then((res) => {
-        if (res.status === 401) {
-          throw new Error('invalid-token');
-        }
-        return res;
-      })
-
-      .then((res) => res.json())
-      .then((data) => setProducts(data.products))
-      .catch((e) => {
-        if (e.message === 'invalid-token') {
-          router.push('/login');
-        }
-      });
-  }, [router]);
-
-  const handleLogout = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const request = await fetch(`http://localhost:3333/api/auth/logout`, {
-      credentials: 'include',
-    });
-
-    if (request.status === 200) {
-      router.push('/login');
-    }
-  };
-
-  return (
-    <div className={styles.page}>
-      <button onClick={handleLogout} className="">
-        Logout
-      </button>
-
-      <ul className="">
-        {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div className={styles.page}>HOME</div>;
 }
 
 export default Index;
---
+++ apps/store/pages/products/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import * as D from '@crea/domain';
+
+import { useRouter } from 'next/router';
+
+export interface ProductsProps {}
+
+export function Products(props: ProductsProps) {
+  const [products, setProducts] = useState<D.Products>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(`http://localhost:3333/api/products`, {
+      credentials: 'include',
+    })
+      .then((res) => {
+        if (res.status === 401) {
+          throw new Error('invalid-token');
+        }
+        return res;
+      })
+
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products))
+      .catch((e) => {
+        if (e.message === 'invalid-token') {
+          router.push('/login');
+        }
+      });
+  }, [router]);
+
+  const handleLogout = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const request = await fetch(`http://localhost:3333/api/auth/logout`, {
+      credentials: 'include',
+    });
+
+    if (request.status === 200) {
+      router.push('/login');
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogout} className="">
+        Logout
+      </button>
+
+      <ul className="">
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
